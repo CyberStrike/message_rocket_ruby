186ca40875b45c80fb5ae652901b81b1e5d7aa5e
James Harton <james@resistor.io>
2014-12-11 10:48:30 +1300

    Add a guard against setting an incorrect channel permission.

--- lib/message_rocket/permit_response.rb
+++ lib/message_rocket/permit_response.rb
@@ -1,5 +1,12 @@
 module MessageRocket
   class PermitResponse < Response
+    PERMISSIONS = %w| read_write read_only write_only |.map(&:to_sym)
+
+    def initialize request, options
+      raise ArgumentError, "Invalid permission argument: must be one of #{PERMISSIONS.map(&:inspect).join(', ')}" unless PERMISSIONS.member? options
+      super
+    end
+
     def response_type
       'permit'
     end
